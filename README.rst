430042738e772799e2c73257fa4632597f6d938a
vwrobel <valerian.wrobel1@gmail.com>
2016-12-03 17:23:00 +0100

    Developed example and README

--- README.rst
+++ README.rst
@@ -1,6 +1,6 @@
-===============================
-Scopethis CVtools
-===============================
+=====================
+[COMING SOON] CVtools
+=====================
 
 
 .. image:: https://img.shields.io/pypi/v/scopethis_cvtools.svg
@@ -18,20 +18,36 @@ Scopethis CVtools
      :alt: Updates
 
 
-CV tools to spot and track objects in a video
+CV tools to classify and track objects in videos.
 
 
 * Free software: MIT license
 * Documentation: https://scopethis-cvtools.readthedocs.io.
 
 
-Features
---------
+What is this package?
+---------------------
+It is a set of python processes (video filters, trackers, classifiers...) that can be chained and called simply with the wrapper function apply_comp_process_to_video.
 
-* TODO
+Processes can be based on any package, it mainly use openCV for the moment.
+
+Its main goal is to process videos on [COMING SOON].
+
+
+How to use this package?
+------------------------
+Check the example.
+
+
+TO DO
+-----
+
+* More processes
+* Develop classifier object
+* GUI
 
 Credits
----------
+-------
 
 This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
 
--- example/example.py
+++ example/example.py
@@ -1,13 +1,28 @@
 import sys
 
-from ..cvtools import apply_comp_process_to_video, SelectionWindow, CompTracker
+from cvtools import apply_comp_process_to_video, SelectionWindow, CompTracker, CompFilter
 
 
-def main():
+def example():
+    # The path of the video you want to work on
     vid_filepath = './example/vids/aouta/aouta.mp4'
+
+    # The filter you want to apply
+    filter_color_select = CompFilter(
+        'filter_color_select',
+        ['color_select'],
+        [{'lower': [17, 15, 100], 'upper': [50, 56, 200]}])
+
+    # The path of the filtered video (.avi for OpenCV writer to work)
+    filtered_vid_filepath = vid_filepath.replace('.mp4', '_filter_color_select.avi')
+
+    apply_comp_process_to_video(vid_filepath, filtered_vid_filepath, filter_color_select)
+
+    # The object you want to track (x, y , w, h are ratios between position and frame dimension)
     selection = SelectionWindow(0.72, 0.74, 0.054, 0.19, 0, (100, 100, 0), 'manual', 'aouta')
     selection_list = [selection]
 
+    # The tracker you want to apply
     tracker_camshift = CompTracker(
         'tracker_camshift',
         ['camshift'],
@@ -16,10 +31,11 @@ def main():
           'color_channel': [2]}],
         selection_list)
 
+    # The path of the tracked video
     tracked_vid_filepath = vid_filepath.replace('.mp4', '_tracker_camshift.avi')
+
     apply_comp_process_to_video(vid_filepath, tracked_vid_filepath, tracker_camshift)
 
 
 if __name__ == '__main__':
-    status = main()
-    sys.exit(status)
+    example()
